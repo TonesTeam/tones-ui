05a7c2eca4eee2c7c84533c1dcd38d3a84c1b14e
TaisiaKozharina <kozharina1306@gmail.com>
2022-06-10 19:02:52 +0300

    Color & size fix.

--- modules/frontend/src/ProtocolList/Recommendations.tsx
+++ modules/frontend/src/ProtocolList/Recommendations.tsx
@@ -6,6 +6,7 @@ import "./Recommendations.css";
 import { ProtocolDto, UsedProtocolLiquid } from 'sharedlib/dto/protocol.dto';
 import { getRequest } from 'common/util';
 import { p1 } from "ProtocolList/ProtocolList";
+import { CheckboxProps } from "@mui/material";
 
 /* const liquids = p1.usedLiquids;  <- TEST DTO*/
 const protocol = (await getRequest<ProtocolDto[]>("/protocol/all")).data[0]
@@ -17,6 +18,27 @@ function createTable() {
     let finalTable = document.querySelector('.tableContainer');
     let recomTable = document.createElement('table');
     recomTable.setAttribute("id", "recom-table");
+
+
+    /*   adding test colgroup  */
+    /* -------------COMMENT TO RETURN TO UNGROUPED TABLE COLOURS ---------- */
+    let colgroup = document.createElement('colgroup');
+
+    let colSm = document.createElement('col');
+    colSm.setAttribute('class', 'small');
+    colSm.setAttribute('span', '5'); 
+    colgroup.appendChild(colSm);
+
+    let colMd = document.createElement('col');
+    colMd.setAttribute('class', 'medium'); 
+    colgroup.appendChild(colMd);
+
+    let colLg = document.createElement('col');
+    colLg.setAttribute('class', 'large'); 
+    colgroup.appendChild(colLg);
+
+    recomTable.appendChild(colgroup);
+    /*-------------------------------------------------------------------*/
 
     /* adding header row */
     let thead = document.createElement('thead');
@@ -34,6 +56,8 @@ function createTable() {
 
     thead.appendChild(headerRow);
     recomTable.appendChild(thead);
+
+
 
     /* adding body */
     let body = document.createElement('tbody');
@@ -61,7 +85,7 @@ function createTable() {
                 nameDiv.appendChild(textName);
 
                 let amountDiv = document.createElement('div');
-                amountDiv.setAttribute("class", "liqName");
+                amountDiv.setAttribute("class", "liqAmount");
                 let textAmount = document.createTextNode(Object(tableLiquids[iter].amount) + ' ml');
                 amountDiv.appendChild(textAmount);
 
@@ -71,10 +95,15 @@ function createTable() {
                 cell.setAttribute("class", "liquidCell");
             }
             else {
-                let text = document.createTextNode('Empty ' + j.toString());
+                let text = document.createTextNode('Empty ');
                 cellContent.appendChild(text);
                 cell.setAttribute("class", "emptyCell");
             }
+
+            if (j==5 && i>3){
+                cell.setAttribute('class', cell.getAttribute('class')+' washing');
+            }
+            
             cell.appendChild(cellContent);
             row.appendChild(cell);
             iter++;
@@ -87,6 +116,19 @@ function createTable() {
     finalTable!.appendChild(recomTable);
 }
 
+/* const showBtn = (event: React.MouseEvent<HTMLInputElement>, check:CheckboxProps) => {
+    var btn = document.getElementById("launchBtn");
+
+    if(check.checked){
+       btn?.setAttribute("style", "visibility: visible");
+    }
+    else{
+       btn?.setAttribute("style", "visibility: hidden");
+    }
+} */
+
+
+
 export default function Recommendations() {
 
     useEffect(() => {
@@ -98,16 +140,22 @@ export default function Recommendations() {
             <NavigationBar />
             <div id="main">
                 <div className="open-menu-btn"></div>
+                <div className="body">
+                    
+                </div>
                 <div className="tableContainer">
                     <h4><i>Fill in tubes and place them according to the configuration table:</i></h4>
                     
                 </div>
-                <div className="confirm">
-                        <input type="checkbox" name="confirm"></input>
-                        <p></p>
+                <div className="launchContainer">
+                    <div className="confirm">
+                            <input type="checkbox" name="confirm" id="confirmCheck"></input> {/* onClick = {showBtn(this)} */}
+                            <p></p>
+                    </div>
+                    <div id="launchBtn" style={{visibility: "hidden"}}>
+                            <a href="#">START</a>
+                    </div>
                 </div>
-
-                
             </div></>
     )
 }
