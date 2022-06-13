9e5c0dae261ecb7a3f52d8f2c209c61afb855783
andris.zacs <andris.zacs@accenture.com>
2022-06-13 14:43:21 +0300

    axios date parse

--- modules/frontend/src/common/util.ts
+++ modules/frontend/src/common/util.ts
@@ -1,4 +1,34 @@
 import axios, { Axios, AxiosResponse } from 'axios'
+import { parseISO } from "date-fns"
+
+const client = axios.create();
+const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?(?:[-+]\d{2}:?\d{2}|Z)?$/;
+
+function isIsoDateString(value: any): boolean {
+    return value && typeof value === "string" && isoDateFormat.test(value);
+}
+
+export function handleDates(body: any) {
+    if (body === null || body === undefined || typeof body !== "object")
+        return body;
+
+    for (const key of Object.keys(body)) {
+        const value = body[key];
+        if (isIsoDateString(value)) {
+            console.log("Parsing ", value)
+            body[key] = parseISO(value);
+        }
+        else if (typeof value === "object") handleDates(value);
+    }
+}
+
+
+client.interceptors.response.use(originalResponse => {
+    handleDates(originalResponse.data);
+    return originalResponse;
+});
+
+
 
 const domain = window.location.origin
 const prefix = "/api"
@@ -6,12 +36,12 @@ const prefix = "/api"
 export async function getRequest<T>(path: string): Promise<AxiosResponse<T, any>> {
     const fullpath = domain + prefix + path
     console.log("GET: " + fullpath)
-    return await axios.get(fullpath)
+    return await client.get(fullpath)
 }
 
 export async function postRequest<T>(path: string, payload: string): Promise<AxiosResponse<T, any>> {
     const fullpath = domain + prefix + path
     console.log("POST: " + fullpath)
-    return await axios.post(fullpath, payload, { validateStatus: null })
+    return await client.post(fullpath, payload, { validateStatus: null })
 }
 
