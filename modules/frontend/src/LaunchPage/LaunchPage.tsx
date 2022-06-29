6a0047b96fb668acaedcf6e1ab5ebe2428fbf3ed
TaisiaKozharina <kozharina1306@gmail.com>
2022-06-29 15:55:46 +0300

    LaunchPage refactoring

--- modules/frontend/src/LaunchPage/LaunchPage.tsx
+++ modules/frontend/src/LaunchPage/LaunchPage.tsx
@@ -2,11 +2,73 @@ import NavigationBar from "NavigationBar/NavigationBar";
 import "NavigationBar/NavigationBar.css";
 import "./LaunchPage.css";
 import "common/style.css";
-import { useEffect, useState } from "react";
+import { ClassicElement, useEffect, useState } from "react";
 
-var someError = false;
+/* var someError = false;
 var forceStopped = false;
-var finished = false;
+var finished = false; */
+
+interface Status {
+    getColor(): string;
+    getMessage(): JSX.Element;
+}
+
+class Ongoing implements Status {
+    getMessage(): JSX.Element {
+        return (
+            <div></div>
+        );
+    }
+    getColor(): string {
+        return "#6191ae";
+    }
+}
+
+class ForceStopped implements Status {
+    getMessage(): JSX.Element {
+        return (
+            <div className="comment-body">
+                <h4 id="comment-header">Protocol have been force-stopped. It cannot be resumed, but you can launch it again.</h4>
+                <button id="toList-btn"><a href="/list">Go to Protocol List</a></button>
+            </div>
+        );
+    }
+    getColor(): string {
+        return "#ddd";
+    }
+}
+class SysError implements Status {
+    getMessage(): JSX.Element {
+        return (
+            <div className="comment-body">
+                <h4 id="comment-header">Some error occured! Please follow the guide below to detect and fix the error.</h4>
+                <ol id="error-guide">
+                    <li>Check if any slots are displaced. If any - fix the placing.</li>
+                    <li>Make sure that sealing slots cover is closed.</li>
+                    <li>Here will be a more precise fixing guide based on the type of error.</li>
+                    <li>Or not. In that case, pretend like this is not your fault and act casual.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sunt eaque.</li>
+                </ol>
+            </div>
+        );
+    }
+    getColor(): string {
+        return "#ae616a";
+    }
+}
+class Finished implements Status {
+    getMessage(): JSX.Element {
+        return (
+            <div className="comment-body">
+                <h4 id="comment-header">Protocol have successfully finished!</h4>
+                <button><a href="/list">Go to Protocol List</a></button>
+            </div>
+        );
+    }
+    getColor(): string {
+        return "#6f8b6c";
+    }
+}
 
 var i = 0;
 var width = 1;
@@ -17,105 +79,11 @@ enum ProtocolState {
     FINISHED
 }
 
-function moveBar() {
-    /* console.log("Error: " + someError + "/ Force stop" + fs); */
-    if (i == 0) {
-        i = 1;
-        var progBar = document.getElementById("progress-bar");
-        var id = setInterval(frame, 1000);
-        function frame() {
-            if (!someError && !forceStopped) {
-                if (width >= 100) {
-                    clearInterval(id);
-                    i = 0;
-                    progBar!.style.background = "#6f8b6c";
-                    finished = true;
-                } else {
-                    width++;
-                    progBar!.style.width = width + "%";
-                }
-            }
-        }
-
-    }
-}
-
-function toggleError() {
-    someError = true;
-
-    document.getElementById("fake-error")!.style.visibility = "hidden";
-    document.getElementById("progress-bar")!.style.background = "#ae616a";
-    // document.getElementById("comment-body")!.innerHTML += fillCommentSection(someError, forceStopped, finished);
-    document.getElementById("comment-body")!.style.visibility = "visible";
-    document.getElementById("resume-btn")!.style.visibility = "visible";
-
-    console.log("Some error: " + someError);
-}
-
-function errorHadler() {
-    someError = false;
-
-    document.getElementById("fake-error")!.style.visibility = "visible";
-    document.getElementById("progress-bar")!.style.background = "#6191ae";
-    //document.getElementById("comment-body")!.innerHTML += fillCommentSection(someError, forceStopped, finished);
-    document.getElementById("comment-body")!.style.visibility = "hidden";
-    document.getElementById("resume-btn")!.style.visibility = "hidden";
-
-    console.log("Resolving, some error: " + someError);
-}
-
-function forceStop() {
-    forceStopped = true;
-
-    document.getElementById("progress-bar")!.style.background = "#ddd";
-    document.getElementById("comment-body")!.style.visibility = "visible";
-
-    console.log("Force stopped: " + forceStopped);
-}
-
-function resolveComment() {
-    if (someError) {
-        document.getElementById("fake-error")!.style.visibility = "hidden";
-        document.getElementById("progress-bar")!.style.background = "#ae616a";
-        document.getElementById("comment-body")!.style.visibility = "visible";
-        document.getElementById("resume-btn")!.style.visibility = "visible";
-
-        /*          return(
-                    <div>
-                        <h4 id="error-header">Some error occured! Please follow the guide below to detect and fix the error.</h4>
-                        <ol id="error-guide">
-                            <li>Check if any slots are displaced. If any - fix the placing.</li>
-                            <li>Make sure that sealing slots cover is closed.</li>
-                            <li>Here will be a more precise fixing guide based on the type of error.</li>
-                            <li>Or not. In that case, pretend like this is not your fault and act casual.</li>
-                            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia sunt eaque.</li>
-                        </ol>
-                    </div>
-                ); */
-    } else if (forceStopped) {
-        document.getElementById("progress-bar")!.style.background = "#ddd";
-        document.getElementById("comment-body")!.style.visibility = "visible";
-        /* return(
-            <div>
-                <h4 id="error-header">Protocol have been force-stopped. It cannot be resumed, but you can launch it again.</h4>
-                <button><a href="/list">Go to Protocol List</a></button>
-            </div>
-        ) */
-    }
-    else if (finished) {
-        /*  return(
-             <div>
-                 <h4>Protocol have successfully finished!</h4>
-                 <button><a href="/list">Go to Protocol List</a></button>
-             </div>
-         ) */
-    }
-}
-
-const stateMessage: Map<ProtocolState, JSX.Element> = new Map([
-    [ProtocolState.FINISHED, <div>Finished</div>],
-    [ProtocolState.ERROR, <div>Some Error</div>],
-    [ProtocolState.FORCE_STOPPED, <div>Force Stopped</div>],
+const launchStatus: Map<ProtocolState, Status> = new Map([
+    [ProtocolState.FINISHED, new Finished()],
+    [ProtocolState.ERROR, new SysError()],
+    [ProtocolState.FORCE_STOPPED, new ForceStopped()],
+    [ProtocolState.ONGOING, new Ongoing()]
 ]);
 
 export default function LaunchPage() {
@@ -146,25 +114,21 @@ export default function LaunchPage() {
                     </div>
 
                     <div id="progress">
-                        <div id="progress-bar" style={{ width: `${progress}%` }}></div>
+                        <div id="progress-bar" style={{ width: `${progress}%`, backgroundColor: launchStatus.get(ps)!.getColor() }}></div>
                     </div>
                 </div>
 
                 <div className="footer">
-                    <div id="comment-body">
-                        {stateMessage.get(ps)}
+                    <div id="comment">
+                        {launchStatus.get(ps)!.getMessage()}
                     </div> {/* to be filled according to current status */}
 
                     <div className="btn-panel">
                         <button onClick={() => setProtocolState(ProtocolState.FORCE_STOPPED)} id="stop-btn">Force stop</button>
-                        <button onClick={() => setProtocolState(ProtocolState.ERROR)} id="fake-error">Toggle fake error</button>
+                        <button onClick={() => setProtocolState(ProtocolState.ERROR)} id="fake-error"
+                            style={{ visibility: ps == ProtocolState.ERROR ? "hidden" : "visible" }}>Toggle fake error</button>
                         <button onClick={() => setProtocolState(ProtocolState.ONGOING)} id="resume-btn"
                             style={{ visibility: ps == ProtocolState.ERROR ? "visible" : "hidden" }}>Resume</button>
-
-
-                        {/*                         <button onClick={() => setForceStop(true)} id="stop-btn">Force stop</button>
-                        <button onClick={() => setError(true)} id="fake-error">Toggle fake error</button>
-                        <button onClick={() => setError(false)} id="resume-btn">Resume</button> */}
                     </div>
                 </div>
             </div>
