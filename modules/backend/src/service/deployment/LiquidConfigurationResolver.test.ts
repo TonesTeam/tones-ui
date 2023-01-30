86b091b85b20c2a5ff8fa55dbb04a6f75b7c9908
andris.zacs <andris.zacs@accenture.com>
2023-01-30 14:52:12 +0200

    test fix

--- modules/backend/src/service/deployment/LiquidConfigurationResolver.test.ts
+++ modules/backend/src/service/deployment/LiquidConfigurationResolver.test.ts
@@ -8,8 +8,8 @@ describe('Resolve liquid configuration', () => {
     it('Single liquid: [50 10]', () => {
         const r = new LiquidConfigurationResolver()
         const conf = r.resolveLiquidConfiguration([
-            new LiquidApplicationCommand(undefined, 1, 50, { id: 1, isWashing: false }),
-            new LiquidApplicationCommand(undefined, 1, 10, { id: 1, isWashing: false }),
+            new LiquidApplicationCommand(undefined, 1, 50, { id: 1, isWashing: false, isWater: false }),
+            new LiquidApplicationCommand(undefined, 1, 10, { id: 1, isWashing: false, isWater: false }),
         ])
         const res = conf.map(d => d.liquidAmount)
         console.log(res);
@@ -19,11 +19,11 @@ describe('Resolve liquid configuration', () => {
     it('2 liquids: [1 2 2], [14 2]', () => {
         const r = new LiquidConfigurationResolver()
         const conf = r.resolveLiquidConfiguration([
-            new LiquidApplicationCommand(undefined, 1, 1, { id: 1, isWashing: false }),
-            new LiquidApplicationCommand(undefined, 1, 2, { id: 1, isWashing: false }),
-            new LiquidApplicationCommand(undefined, 1, 2, { id: 1, isWashing: false }),
-            new LiquidApplicationCommand(undefined, 1, 14, { id: 2, isWashing: false }),
-            new LiquidApplicationCommand(undefined, 1, 2, { id: 2, isWashing: false }),
+            new LiquidApplicationCommand(undefined, 1, 1, { id: 1, isWashing: false, isWater: false }),
+            new LiquidApplicationCommand(undefined, 1, 2, { id: 1, isWashing: false, isWater: false }),
+            new LiquidApplicationCommand(undefined, 1, 2, { id: 1, isWashing: false, isWater: false }),
+            new LiquidApplicationCommand(undefined, 1, 14, { id: 2, isWashing: false, isWater: false }),
+            new LiquidApplicationCommand(undefined, 1, 2, { id: 2, isWashing: false, isWater: false }),
         ])
         const liquidsConf = groupByMapped(conf, d => d.liquidId, d => d.liquidAmount)
         expect(liquidsConf.get(1)).to.have.ordered.members([5])
@@ -33,9 +33,9 @@ describe('Resolve liquid configuration', () => {
     it('1 liquid: [25 30 5]', () => {
         const r = new LiquidConfigurationResolver()
         const conf = r.resolveLiquidConfiguration([
-            new LiquidApplicationCommand(undefined, 1, 25, { id: 1, isWashing: false }),
-            new LiquidApplicationCommand(undefined, 1, 30, { id: 1, isWashing: false }),
-            new LiquidApplicationCommand(undefined, 1, 5, { id: 1, isWashing: false }),
+            new LiquidApplicationCommand(undefined, 1, 25, { id: 1, isWashing: false, isWater: false }),
+            new LiquidApplicationCommand(undefined, 1, 30, { id: 1, isWashing: false, isWater: false }),
+            new LiquidApplicationCommand(undefined, 1, 5, { id: 1, isWashing: false, isWater: false }),
         ])
         const liquidsConf = groupByMapped(conf, d => d.liquidId, d => d.liquidAmount)
         expect(liquidsConf.get(1)).to.have.ordered.members([50, 50])
