5a00045b96d49e4b437ffcc044e9e7c9304ad382
eriks47 <eriksmuhins@gmail.com>
2025-08-31 20:08:39 +0300

    ui: update protocol list item compinent design

--- frontend_native/Pages/ProtocolList.tsx
+++ frontend_native/Pages/ProtocolList.tsx
@@ -1,6 +1,5 @@
 import {
     StyleSheet,
-    Text,
     View,
     TouchableWithoutFeedback,
     TextInput,
@@ -9,6 +8,7 @@ import {
     Image,
     TouchableOpacity,
 } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 import {
     AppStyles,
     MainContainer,
@@ -17,17 +17,10 @@ import {
 import NavBar from '../navigation/CustomNavigator';
 import { ProtocolDto } from 'common/dto/protocol.dto';
 import { useEffect, useState, useRef } from 'react';
-import { useAppDispatch, useAppSelector } from '../state/hooks';
 import { getRequest, makeRequest } from '../common/util';
 import Txt from '../components/Txt';
 import ConfirmationModal from '../common/TonesModal';
-import Search_Icon from '../assets/icons/search.svg';
-import Arrow_Icon from '../assets/icons/arrow-down.svg';
 import NotFound_Icon from '../assets/icons/question.svg';
-import Launch_btn_Icon from '../assets/icons/launch_btn.svg';
-import Template_btn_Icon from '../assets/icons/template_btn.svg';
-import Edit_btn_Icon from '../assets/icons/edit_btn.svg';
-import Delete_btn_Icon from '../assets/icons/delete_btn.svg';
 import { ScrollView } from 'react-native-gesture-handler';
 import {
     NativeStackNavigationProp,
@@ -35,333 +28,165 @@ import {
 } from '@react-navigation/native-stack';
 import { Method } from 'axios';
 import InfoModal from '../components/InfoModal';
-import { useIsFocused } from '@react-navigation/native';
 import { InfoType } from '../common/types';
-import { TrashIcon } from '@gluestack-ui/themed';
 import {
     Input,
     InputField,
     InputSlot,
     InputIcon,
     Pressable,
+    Button,
+    ButtonText,
+    Icon,
+    Box,
+    VStack,
+    HStack,
+    Text,
+    Divider,
 } from '@gluestack-ui/themed';
+import GeneratedAvatar from '../components/GeneratedAvatar';
+import {
+    X,
+    SearchIcon,
+    Rocket,
+    File,
+    Edit3,
+    Trash,
+    ArrowDown,
+} from 'lucide-react-native';
 
-import { X, SearchIcon } from 'lucide-react-native';
-
-function ProtocolItem(props: {
+function ProtocolItem({
+    protocol,
+    navigation,
+    toggleDeletionModal,
+}: {
     protocol: ProtocolDto;
     navigation: NativeStackNavigationProp<any>;
     toggleDeletionModal: (val: boolean) => void;
 }) {
     const [deleteModal, setDeleteModal] = useState(false);
-    const [expanded, setExpanded] = useState(false);
-    //const dispatch = useAppDispatch();
-    let disableLaunch = useAppSelector((state) => state.isRunning);
-    let protoInList = useAppSelector((state) => state.protocols).find(
-        (e) => e.protocol.id == props.protocol.id,
-    );
-
-    let protocolStatus: string;
-    if (protoInList != undefined) {
-        switch (protoInList.status) {
-            case 'ONGOING':
-                protocolStatus = 'Ongoing';
-                break;
-            case 'ERROR':
-                protocolStatus = 'Error occured';
-                break;
-            case 'FINISHED':
-                protocolStatus = 'Finished';
-                break;
-            default:
-                protocolStatus = 'Undefined';
-        }
-    } else if (useAppSelector((state) => state.protocols).length == 0) {
-        protocolStatus = 'Ready to launch';
-    } else {
-        protocolStatus = 'Launch prohibited';
-    }
-
-    const animatedController = useRef(new Animated.Value(0)).current;
-    const [bodySectionHeight, setBodySectionHeight] = useState(0);
-
-    const bodyHeight = animatedController.interpolate({
-        inputRange: [0, 1],
-        outputRange: [0, bodySectionHeight],
-    });
-
-    const arrowAngle = animatedController.interpolate({
-        inputRange: [0, 1],
-        outputRange: ['0rad', `${Math.PI}rad`],
-    });
-
-    const toggleListItem = () => {
-        Animated.timing(animatedController, {
-            toValue: expanded ? 0 : 1,
-            duration: 400,
-            easing: Easing.bezier(0.4, 0.0, 0.2, 1),
-            useNativeDriver: false,
-        }).start();
-
-        setExpanded(!expanded);
-    };
 
     const deleteProtocol = (id: number) => {
         makeRequest('DELETE' as Method, `/protocol/delete/${id}`)
             .then((r) => {
                 if (r.status >= 200 && r.status <= 299) {
-                    props.toggleDeletionModal(true);
+                    toggleDeletionModal(true);
                 } else {
-                    props.toggleDeletionModal(false);
+                    toggleDeletionModal(false);
                 }
             })
             .catch((err) => {
                 console.log(err.message);
-                props.toggleDeletionModal(false);
+                toggleDeletionModal(false);
             });
     };
 
-    const ps = StyleSheet.create({
-        proto_box: {
-            width: '100%',
-            height: 'auto',
-            borderRadius: 8,
-            backgroundColor: AppStyles.color.elem_back,
-            paddingHorizontal: '2%',
-            paddingVertical: 1,
-            marginBottom: 20,
-        },
-        header: {
-            flexDirection: 'row',
-            paddingVertical: '2%',
-        },
-
-        header_box: {
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-        },
-
-        bold_label: {
-            fontFamily: 'Roboto-bold',
-            fontSize: 16,
-            marginLeft: 5,
-        },
-
-        thin_label: {
-            fontFamily: 'Roboto-thin',
-            fontSize: 16,
-        },
-
-        status_span: {
-            width: 'auto',
-            backgroundColor: AppStyles.color.primary_faded,
-            paddingVertical: '1%',
-            paddingHorizontal: '4%',
-            borderRadius: 15,
-            marginLeft: 10,
-            alignItems: 'center',
-        },
-
-        body_container: {
-            position: 'absolute',
-            bottom: 0,
-            width: '100%',
-            paddingVertical: 10,
-        },
-
-        body: {
-            overflow: 'hidden',
-            height: bodyHeight,
-            alignItems: 'center',
-            borderTopColor: AppStyles.color.background,
-            borderTopWidth: 1,
-            borderBottomColor: AppStyles.color.background,
-            borderBottomWidth: 1,
-        },
-
-        button_panel: {
-            flex: 1,
-            marginTop: 20,
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-        },
-
-        button: {
-            borderRadius: 8,
-            padding: 10,
-            width: '23%',
-            backgroundColor: AppStyles.color.elem_back,
-            alignItems: 'center',
-            flexDirection: 'row',
-            justifyContent: 'center',
-            borderWidth: 1,
-            borderColor: AppStyles.color.background,
-        },
-    });
-
     return (
-        <View
-            style={[
-                ps.proto_box,
-                expanded && {
-                    borderColor: AppStyles.color.primary,
-                    borderWidth: 1,
-                },
-            ]}
+        <Box
+            borderWidth={1}
+            borderColor="$borderLight200"
+            rounded="$lg"
+            bg="$backgroundLight0"
+            p="$4"
+            mb="$3"
+            shadowColor="$borderLight100"
+            shadowOffset={{ width: 0, height: 1 }}
+            shadowOpacity={0.05}
+            shadowRadius={2}
         >
-            <TouchableWithoutFeedback onPress={() => toggleListItem()}>
-                <View style={ps.header}>
-                    <View style={ps.header_box}>
-                        <Txt style={ps.thin_label}>ID:</Txt>
-                        <Txt style={ps.bold_label}>{props.protocol.id}</Txt>
-                        <View style={ps.status_span}>
-                            <Txt
-                                style={{
-                                    fontSize: 12,
-                                    fontFamily: 'Roboto-bold',
-                                    color: AppStyles.color.primary,
-                                }}
-                            >
-                                Ready to launch
-                            </Txt>
-                        </View>
-                    </View>
-                    <View style={ps.header_box}>
-                        <Txt style={ps.thin_label}>Name:</Txt>
-                        <Txt style={ps.bold_label}>{props.protocol.name}</Txt>
-                    </View>
-                    <View style={ps.header_box}>
-                        <Txt style={ps.thin_label}>Author:</Txt>
-                        <Txt style={ps.bold_label}>{props.protocol.author}</Txt>
-                    </View>
-                    <View style={ps.header_box}>
-                        <Txt style={ps.thin_label}>Date of creation:</Txt>
-                        <Txt style={ps.bold_label}>
-                            {new Date(
-                                props.protocol.creationDate,
-                            ).toLocaleDateString()}
-                        </Txt>
-                    </View>
-                </View>
-            </TouchableWithoutFeedback>
-            <Animated.View style={ps.body}>
-                <View
-                    onLayout={(event) => {
-                        setBodySectionHeight(event.nativeEvent.layout.height);
-                    }}
-                    style={ps.body_container}
+            {/* Header */}
+            <HStack alignItems="center" space="md">
+                <GeneratedAvatar name={protocol.name} size={40} />
+                <VStack flex={1} space="xs">
+                    <Text bold size="md">
+                        {protocol.name}
+                    </Text>
+                    <Text size="xs" color="$textLight500">
+                        by {protocol.author} ·{' '}
+                        {new Date(protocol.creationDate).toLocaleDateString()}
+                    </Text>
+                </VStack>
+                <Box px="$2.5" py="$1" rounded="$full" bg="$primary100">
+                    <Text size="xs" bold color="$primary600">
+                        Ready to launch
+                    </Text>
+                </Box>
+            </HStack>
+
+            {/* Body */}
+            {protocol.description && (
+                <Text size="sm" color="$textLight600" mt="$3">
+                    {protocol.description}
+                </Text>
+            )}
+            {!protocol.description && (
+                <Text size="sm" color="$textLight400" mt="$3" italic>
+                    No description provided
+                </Text>
+            )}
+
+            {/* Actions */}
+            <HStack justifyContent="flex-end" space="sm">
+                <Button
+                    size="sm"
+                    bg="$primary500"
+                    onPress={() =>
+                        navigation.navigate('Launch', {
+                            protocol_ID: protocol.id,
+                        })
+                    }
                 >
-                    {props.protocol.description ? (
-                        <Txt>Description: {props.protocol.description}</Txt>
-                    ) : (
-                        <Txt style={s.no_description}>
-                            no description provided
-                        </Txt>
-                    )}
-                    <View style={ps.button_panel}>
-                        <TouchableOpacity
-                            style={[
-                                ps.button,
-                                { backgroundColor: AppStyles.color.primary },
-                            ]}
-                            onPress={() =>
-                                props.navigation.navigate('Launch', {
-                                    protocol_ID: props.protocol.id,
-                                })
-                            }
-                        >
-                            <Launch_btn_Icon
-                                width={20}
-                                height={20}
-                                stroke={'#fff'}
-                            />
-                            <Txt
-                                style={{
-                                    marginLeft: 8,
-                                    color: AppStyles.color.elem_back,
-                                }}
-                            >
-                                Prepare to Launch
-                            </Txt>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={ps.button}
-                            onPress={() =>
-                                props.navigation.navigate('Create protocol', {
-                                    protocol_ID: props.protocol.id,
-                                    preserveID: false,
-                                })
-                            }
-                        >
-                            <Template_btn_Icon
-                                width={20}
-                                height={20}
-                                stroke={AppStyles.color.text_primary}
-                            />
-                            <Txt style={{ marginLeft: 8 }}>Use as Template</Txt>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={ps.button}
-                            onPress={() =>
-                                props.navigation.navigate('Create protocol', {
-                                    protocol_ID: props.protocol.id,
-                                    preserveID: true,
-                                })
-                            }
-                        >
-                            <Edit_btn_Icon
-                                width={20}
-                                height={20}
-                                stroke={AppStyles.color.text_primary}
-                            />
-                            <Txt style={{ marginLeft: 8 }}>Edit protocol</Txt>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={ps.button}
-                            onPress={() => setDeleteModal(true)}
-                        >
-                            <Delete_btn_Icon
-                                width={20}
-                                height={20}
-                                stroke={AppStyles.color.warning}
-                            />
-                            <Txt style={{ marginLeft: 8 }}>Delete</Txt>
-                        </TouchableOpacity>
-                        <ConfirmationModal
-                            isOpen={deleteModal}
-                            onClose={() => setDeleteModal(false)}
-                            action={() => deleteProtocol(props.protocol.id)}
-                            icon={TrashIcon}
-                            headline={`Delete protcol "${props.protocol.name}"`}
-                            text={
-                                'Are you sure you want to delete this protocol? This action cannot be undone.'
-                            }
-                            actionButtonText="Delete"
-                            type="error"
-                        />
-                    </View>
-                </View>
-            </Animated.View>
-            <TouchableOpacity
-                style={{
-                    width: '100%',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-                onPress={() => toggleListItem()}
-            >
-                <Animated.View style={{ transform: [{ rotateX: arrowAngle }] }}>
-                    <Arrow_Icon
-                        height={40}
-                        width={60}
-                        stroke={AppStyles.color.text_faded}
-                    />
-                </Animated.View>
-            </TouchableOpacity>
-        </View>
+                    <Icon as={Rocket} mr="$2" color="white" />
+                    <ButtonText color="white">Launch</ButtonText>
+                </Button>
+                <Button
+                    size="sm"
+                    variant="outline"
+                    onPress={() =>
+                        navigation.navigate('Create protocol', {
+                            protocol_ID: protocol.id,
+                            preserveID: false,
+                        })
+                    }
+                >
+                    <Icon as={File} mr="$2" />
+                    <ButtonText>Template</ButtonText>
+                </Button>
+                <Button
+                    size="sm"
+                    variant="outline"
+                    onPress={() =>
+                        navigation.navigate('Create protocol', {
+                            protocol_ID: protocol.id,
+                            preserveID: true,
+                        })
+                    }
+                >
+                    <Icon as={Edit3} mr="$2" />
+                    <ButtonText>Edit</ButtonText>
+                </Button>
+                <Button
+                    size="sm"
+                    variant="outline"
+                    borderColor="$error500"
+                    onPress={() => setDeleteModal(true)}
+                >
+                    <Icon as={Trash} mr="$2" color="$error500" />
+                    <ButtonText color="$error500">Delete</ButtonText>
+                </Button>
+            </HStack>
+
+            <ConfirmationModal
+                isOpen={deleteModal}
+                onClose={() => setDeleteModal(false)}
+                action={() => deleteProtocol(protocol.id)}
+                icon={Trash}
+                headline={`Delete protocol "${protocol.name}"`}
+                text="Are you sure you want to delete this protocol? This action cannot be undone."
+                actionButtonText="Delete"
+                type="error"
+            />
+        </Box>
     );
 }
 
