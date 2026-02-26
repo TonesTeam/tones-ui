4d47e565c78d8c938a78922bf53ed4ba554ceffb
eriks47 <eriksmuhins@gmail.com>
2026-02-27 01:58:06 +0200

    timeline: fetch estimated protocol time from backend

--- frontend_native/Pages/ProtocolConstructor/Timeline.tsx
+++ frontend_native/Pages/ProtocolConstructor/Timeline.tsx
@@ -1,6 +1,9 @@
 import { StepGroupWithStepsDTO } from 'common/dto/protocol.dto';
 import { Text, HStack, VStack, ScrollView } from '@gluestack-ui/themed';
 import StepGroup from './StepGroup';
+import { useEffect, useState } from 'react';
+import { getRequest, makeRequest } from '../../common/util';
+import { Method } from 'axios';
 
 interface TimelineProps {
     stepGroups: StepGroupWithStepsDTO[];
@@ -15,14 +18,53 @@ const Timeline = ({
     activeStepGroup,
     setActiveStepGroup,
 }: TimelineProps) => {
+    const [estimatedExectuionTime, setEstimatedExecutionTime] = useState(
+        '' as '' | number,
+    );
+
+    useEffect(() => {
+        const payload = JSON.stringify(
+            stepGroups.map((sg) => ({
+                name: sg.step_group.name,
+                description: '',
+                sequence_number: sg.step_group.sequence_number,
+                steps: sg.steps.map((s) => ({
+                    iterations: 1,
+                    sequence_number: s.sequence_number,
+                    target_temperature: s.targetTemperature,
+                    incubation_time: s.incubation_time,
+                    applied_liquid_id: s.applied_liquid_id,
+                    washing_iterations: s.washing_iterations,
+                })),
+            })),
+        );
+
+        console.log('payload', payload);
+
+        const res = makeRequest(
+            'POST' as Method,
+            '/estimated-execution-time',
+            payload,
+        );
+
+        res.then((contents) => {
+            console.log(contents.status);
+            console.log(contents.data);
+            setEstimatedExecutionTime(
+                contents.data['estimated_execution_time'] || 0,
+            );
+        });
+    }, [stepGroups]);
+
     return (
         <VStack>
             <HStack>
                 <Text color="black" fontSize={16}>
                     Timeline
                 </Text>
+                {/* TODO: do some neat time formatting? or just do like in the mockup? */}
                 <Text color="black" fontSize={16} ml="auto">
-                    00:8:00
+                    {estimatedExectuionTime} sec
                 </Text>
             </HStack>
             <ScrollView mt={24} showsVerticalScrollIndicator={false}>
