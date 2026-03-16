fdc3e5daf28e831117786d2628b38417927a235a
eriks47 <eriksmuhins@gmail.com>
2026-03-16 02:20:17 +0200

    dashboard: implement functioning dashboard by the mockup

--- frontend_native/Pages/Dashboard.tsx
+++ frontend_native/Pages/Dashboard.tsx
@@ -1,25 +1,176 @@
-import { Box, Text } from '@gluestack-ui/themed';
+import {
+    Box,
+    Text,
+    Button,
+    ButtonText,
+    ButtonIcon,
+    HStack,
+    VStack,
+} from '@gluestack-ui/themed';
 import { MainContainer, globalElementStyle } from '../constants/styles';
 import NavBar from '../navigation/NavBar';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { ArrowRight, Plus } from 'lucide-react-native';
+import { useEffect, useState } from 'react';
+import { makeRequest } from '../common/util';
+import { Method } from 'axios';
+import { ProtocolDto } from 'common/dto/protocol.dto';
+import ListItem from './ProtocolList/ListItem';
 
 const Dashboard = ({ route, navigation }: NativeStackScreenProps<any>) => {
-    console.log('Dashboard rendered with route:', route.name);
+    const [protocolCount, setProtocolCount] = useState(0);
+    const [liquidCount, setLiquidCount] = useState(0);
+    const [jobCount, setJobCount] = useState(0);
+    const [protocols, setProtocols] = useState([] as ProtocolDto[]);
+
+    useEffect(() => {
+        makeRequest('GET' as Method, '/protocols').then((response) => {
+            setProtocolCount(response.data.length);
+            setProtocols(response.data);
+        });
+        makeRequest('GET' as Method, '/liquids').then((response) => {
+            setLiquidCount(response.data.length);
+        });
+        makeRequest('GET' as Method, '/jobs').then((response) => {
+            setJobCount(response.data.length);
+        });
+    }, []);
+
     return (
         <MainContainer>
             <NavBar />
             <Box flex={1} p={24}>
-                <Text
-                    color="black"
-                    fontSize={32}
-                    fontFamily="Orbitron-Medium"
-                    mb="$8"
-                    mt={16}
+                <HStack
+                    alignItems="flex-start"
+                    justifyContent="space-between"
+                    width="100%"
+                    space="lg"
                 >
-                    Dashboard
-                </Text>
+                    <Text
+                        color="black"
+                        fontSize={32}
+                        fontFamily="Orbitron-Medium"
+                        mb="$8"
+                        mt={16}
+                    >
+                        Dashboard
+                    </Text>
+                    <Button
+                        variant="outline"
+                        rounded="$full"
+                        borderColor="$black"
+                        bg="#1F2832"
+                        mt="$3"
+                        mr="$1"
+                        onPress={() => navigation.navigate('Create protocol')}
+                        alignItems="center"
+                        justifyContent="center"
+                        height={48}
+                    >
+                        <Box
+                            style={{
+                                filter: 'drop-shadow(0 0 6px rgba(255, 255, 255, 0.7)) drop-shadow(0 0 10px rgba(255, 255, 255, 0.5))',
+                            }}
+                        >
+                            <ButtonIcon
+                                bg="transparent"
+                                color="white"
+                                as={Plus}
+                                mr="$1"
+                            />
+                        </Box>
+                        <ButtonText color="white" fontSize={14}>
+                            Create New Protocol
+                        </ButtonText>
+                    </Button>
+                </HStack>
+                <VStack gap={10}>
+                    <Text
+                        color="black"
+                        fontSize={16}
+                        fontFamily="Orbitron-SemiBold"
+                        opacity={0.4}
+                    >
+                        Key metrics
+                    </Text>
+                    <HStack gap={30}>
+                        <InfoCard title="Protocols" value={protocolCount} />
+                        <InfoCard title="Launches" value={jobCount} />
+                        <InfoCard title="Library" value={liquidCount} />
+                    </HStack>
+                </VStack>
+                <VStack gap={10} mt={40}>
+                    <HStack alignItems="center" justifyContent="space-between">
+                        <Text
+                            color="black"
+                            fontSize={16}
+                            fontFamily="Orbitron-SemiBold"
+                            opacity={0.4}
+                        >
+                            Latest protocols
+                        </Text>
+                        <Button
+                            onPress={() => navigation.navigate('Protocols')}
+                            bg="transparent"
+                        >
+                            <ButtonIcon
+                                as={ArrowRight}
+                                color="black"
+                                size={12}
+                            />
+                            <ButtonText
+                                ml="$1"
+                                fontSize={12}
+                                fontFamily="Manrope-Medium"
+                                color="black"
+                            >
+                                View all
+                            </ButtonText>
+                        </Button>
+                    </HStack>
+                    <VStack>
+                        {protocols.slice(0, 3).map((protocol) => {
+                            return (
+                                <ListItem
+                                    navigation={navigation}
+                                    protocol={protocol}
+                                    removeProtocolFromList={() => {}}
+                                />
+                            );
+                        })}
+                    </VStack>
+                </VStack>
             </Box>
         </MainContainer>
+    );
+};
+
+const InfoCard = ({ title, value }: { title: string; value: number }) => {
+    return (
+        <Box
+            bg="white"
+            borderRadius="$xl"
+            p="$4"
+            width={230}
+            height={200}
+            style={{
+                filter: 'drop-shadow(0 2px 4px rgba(0, 0, 0, 0.1))',
+            }}
+        >
+            <Text color="black" fontSize={16} fontFamily="Manrope-SemiBold">
+                {title}
+            </Text>
+            <Box alignSelf="center" justifyContent="center" height="100%">
+                <Text
+                    color="black"
+                    mb={35}
+                    fontSize={40}
+                    fontFamily="Manrope-Medium"
+                >
+                    {value}
+                </Text>
+            </Box>
+        </Box>
     );
 };
 
